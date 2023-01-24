869516ab9b96ced0b1b3e3efa91b3bb49b37d763
Francois973 <robino.francois973@gmail.com>
2023-01-24 10:22:39 +0100

    chore: add controller posts and it works also like moderable models controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
   resources :moderable_models, only: %i[index new create]
   resources :posts, only: %i[index new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
